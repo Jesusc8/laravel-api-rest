d408f80a5692bebcc7649a6fcb259f9391cc5881
Jesus Castro Java <jesusmcastror@gmail.com>
2025-11-08 12:10:14 +0100

    [Add] Create a routes and controllers to get the data

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\TagController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('categories',            [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+
+Route::get('recipes',          [RecipeController::class, 'index']);
+Route::get('recipes/{recipe}', [RecipeController::class, 'show']);
+
+
+Route::get('tags',      [TagController::class, 'index']);
+Route::get('tags/{tag}',[TagController::class, 'show']);
