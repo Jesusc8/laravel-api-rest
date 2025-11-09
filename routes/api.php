2dda8b4ec2e21566624046a6c7fdb4cfda53fa31
Jesus Castro Java <jesusmcastror@gmail.com>
2025-11-09 18:30:29 +0100

    [Add] Authentication for all the created routes.

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,21 @@ use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('categories',            [CategoryController::class, 'index']);
-Route::get('categories/{category}', [CategoryController::class, 'show']);
 
-Route::apiResource('recipes',  RecipeController::class);
-
-
-// Route::get('recipes',               [RecipeController::class, 'index']);
-// Route::post('recipes',              [RecipeController::class, 'store']);
-// Route::get('recipes/{recipe}',      [RecipeController::class, 'show']);
-// Route::put('recipes/{recipe}',      [RecipeController::class, 'update']);
-// Route::delete('recipes/{recipe}',   [RecipeController::class, 'destroy']);
-
-
-Route::get('tags',      [TagController::class, 'index']);
-Route::get('tags/{tag}',[TagController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('categories',            [CategoryController::class, 'index']);
+    Route::get('categories/{category}', [CategoryController::class, 'show']);
+    
+    Route::apiResource('recipes',  RecipeController::class);
+    
+    
+    // Route::get('recipes',               [RecipeController::class, 'index']);
+    // Route::post('recipes',              [RecipeController::class, 'store']);
+    // Route::get('recipes/{recipe}',      [RecipeController::class, 'show']);
+    // Route::put('recipes/{recipe}',      [RecipeController::class, 'update']);
+    // Route::delete('recipes/{recipe}',   [RecipeController::class, 'destroy']);
+    
+    
+    Route::get('tags',      [TagController::class, 'index']);
+    Route::get('tags/{tag}',[TagController::class, 'show']);
+});
