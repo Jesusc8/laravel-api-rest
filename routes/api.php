4e3641ba13c26dbd1ed5fe730c0e739aa47c7e61
Jesus Castro Java <jesusmcastror@gmail.com>
2025-11-09 19:28:12 +0100

    [Add] Create route and controller to log in using API

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\TagController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\LoginController;
+
+
+Route::get('login', [LoginController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('categories',            [CategoryController::class, 'index']);
